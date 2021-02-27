274e8358162f1cb69dbef27d3b8673fc5022eb23
Chadwell-Clark <wcclark23@comcast.net>
2021-02-27 11:29:03 -0600

    created seeds ready to plant?

--- src/scripts/main.js
+++ src/scripts/main.js
@@ -1,9 +1,22 @@
 console.log("Welcome to the main module")
 import { createPlan } from "./plan.js";
+import { createAsparagus } from "./seeds/asparagus.js";
+import { createCorn } from "./seeds/Corn.js";
+import { createPotato } from "./seeds/Potato.js";
+import { createSoybean } from "./seeds/Soybean.js";
+import { createWheat } from "./seeds/Wheat.js";
+import { createSunflower } from "./seeds/Sunflower.js";
+
 
 const yearlyPlan  = createPlan();
-
+// const asparagusSeed = createAsparagus();
 console.log(yearlyPlan);
 
+console.log(createAsparagus());
+console.log(createCorn());
+console.log(createSoybean());
+console.log(createWheat());
+console.log(createSunflower());
+console.log(createPotato());
 
 
